4f48a07c829a146a1b70080b8665f098ee1d2476
Daniel Montero <danielmontero.cr@gmail.com>
2024-09-15 02:55:58 +0000

    Feat: add devices list

---
+++ pages/Listing details/jsobjects/functions/functions.js
@@ -0,0 +1,21 @@
+export default {
+	Switch1onChange (listingId) {
+		if (Switch1.isSwitchedOn) {
+			console.log("Listing is disabled, enable it")
+			Listing_Enable.run({ listingId })
+				.then((response) => {
+					showAlert("Listing enabled")
+					return response
+				}) // run after the query is successful
+        .catch(() => {showAlert("Error: listing not enabled")}) // run if the query encounters any errors
+		} else {
+			console.log("Listing is enabled, disable it")
+			Listing_Disable.run({ listingId })
+				.then((response) => {
+					showAlert("Listing disabled")
+					return response
+				}) // run after the query is successful
+        .catch(() => {showAlert("Error: listing not disabled")}) // run if the query encounters any errors
+		}
+	}
+}
