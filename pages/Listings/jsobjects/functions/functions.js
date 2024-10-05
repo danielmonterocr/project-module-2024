28c3ce856e2c400d0dec6fcaf69bd6140c694f51
Daniel Montero <danielmontero.cr@gmail.com>
2024-10-05 21:36:34 +0000

    Feat: add filter to reports.

--- pages/Listings/jsobjects/functions/functions.js
+++ pages/Listings/jsobjects/functions/functions.js
@@ -1,23 +1,28 @@
 export default {
+	FetchListings () {
+		const title = "";
+		const provider = "";
+		Listings_Fetch.run({ title, provider });
+	},
 	Switch1onChange (listingId) {
 		if (!stw_enable.isSwitchedOn) {
 			console.log("Listing is disabled, enable it")
 			Listing_Enable.run({ listingId })
 				.then((response) => {
-					showAlert("Listing enabled")
-					stw_enable.setValue(true);
-					return response
-				}) // run after the query is successful
-        .catch(() => {showAlert("Error: listing not enabled")}) // run if the query encounters any errors
+				showAlert("Listing enabled")
+				stw_enable.setValue(true);
+				return response
+			}) // run after the query is successful
+				.catch(() => {showAlert("Error: listing not enabled")}) // run if the query encounters any errors
 		} else {
 			console.log("Listing is enabled, disable it")
 			Listing_Disable.run({ listingId })
 				.then((response) => {
-					showAlert("Listing disabled")
-					stw_enable.setValue(false);
-					return response
-				}) // run after the query is successful
-        .catch(() => {showAlert("Error: listing not disabled")}) // run if the query encounters any errors
+				showAlert("Listing disabled")
+				stw_enable.setValue(false);
+				return response
+			}) // run after the query is successful
+				.catch(() => {showAlert("Error: listing not disabled")}) // run if the query encounters any errors
 		}
 	},
 	NavigateToDevices (listingId) {
